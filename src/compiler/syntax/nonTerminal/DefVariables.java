23d42f5aea9a50f6a4c6cd0bdcf2f04b7cae92cc
YassineMarroun <el.yasin@hotmail.com>
2022-04-02 17:04:21 +0200

    Commit

---
+++ src/compiler/syntax/nonTerminal/DefVariables.java
@@ -0,0 +1,17 @@
+package compiler.syntax.nonTerminal;
+
+import java.util.ArrayList;
+
+import compiler.semantic.symbol.SymbolVariable;
+
+import java.util.ArrayList;
+
+import compiler.semantic.symbol.SymbolVariable;
+
+public class DefVariables extends NonTerminal {
+
+    private ArrayList<SymbolVariable> variables;
+    ArrayList<SelectorValorOReferencia> identificadores;
+
+    
+}
