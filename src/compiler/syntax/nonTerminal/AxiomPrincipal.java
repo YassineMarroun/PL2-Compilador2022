e9607343f8f9825f5a445c4ef7c4fb812456a2c9
YassineMarroun <el.yasin@hotmail.com>
2022-04-12 17:05:55 +0200

    Commit

---
+++ src/compiler/syntax/nonTerminal/AxiomPrincipal.java
@@ -0,0 +1,5 @@
+package compiler.syntax.nonTerminal;
+
+public class AxiomPrincipal extends Axiom{
+    
+}
---
+++ src/compiler/syntax/nonTerminal/Funcion.java
@@ -0,0 +1,41 @@
+package compiler.syntax.nonTerminal;
+
+import java.util.ArrayList;
+
+public class Funcion extends NonTerminal {
+
+    private ArrayList<Sentencia> sentencias;
+
+    public Funcion(ArrayList<Sentencia> sentencias, Sentencia sentencia) {
+        super();
+        this.sentencias = sentencias;
+        this.sentencias.add(0, sentencia);
+    }
+
+    public int comprobarDevolver(ArrayList<Sentencia> sentencias) {
+        
+        int numeroDevolver = 0;
+
+        for(Sentencia s : sentencias) {
+            if(s instanceof SentenciaDevolver) {
+                numeroDevolver++;
+            }
+
+            if(s instanceof SentenciaIf) {
+                ArrayList<Sentencia> sIF = ((SentenciaIf)s).getSentenciasIF();
+                ArrayList<Sentencia> sElse = ((SentenciaIf)s).getSentenciasElse();
+                numeroDevolver += comprobarDevolver(sIF) + comprobarDevolver(sElse);
+            } else if(s instanceof SentenciaFor) {
+                ArrayList<Sentencia> sFor = ((SentenciaFor)s).getSentencias();
+
+            }
+
+            // Si es un if o un for, comprobar su listado de sentencias
+        }
+        return numeroDevolver;
+    }
+    
+    public ArrayList<Sentencia> getSentencias() {
+        return sentencias;
+    }
+}
